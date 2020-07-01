3412db22cfcd340be2164dc99c216709512af599
Paulo Campos <pauloalexbh@gmail.com>
2020-07-01 15:07:40 -0300

    Update InterruptionDoor.py

--- InterruptionDoor.py
+++ InterruptionDoor.py
@@ -2,33 +2,45 @@
 # -*- coding: utf-8 -*-
  
 import Rpi.GPIO as gpio
+from datetime import datetime
 import time
+
  
  
 """ Global """
-PIN=23
+Porta=23
+PortaEvento = datetime.now()
+string PortaEstado = "ND"
  
 """ Funcoes """
-def action_press_button(gpio_pin):
-    print "Você pressionou o botão do pino %d e agora ele sera desativado" % gpio_pin
+def action_event_button(gpio_pin):{
+  if gpio.input(Porta) == 0:
+   PortaEstado = "aberta";
+ else:
+   PortaEstado = "fechada";
  
+ PortaEvento = datetime.now()
+ #datahora = datetime.now()
+ print "Houve um evento na porta (pino %d) e agora ela está %s "\n" % gpio_pin, %PortaEstado
+ print (datahora)
+}
  
 """ Configurando GPIO """
 # Configurando o modo do GPIO como BCM
 gpio.setmode(gpio.BCM)
  
 # Configurando PIN's como INPUT e modo pull-down interno
-gpio.setup(PIN, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(Porta, gpio.IN, pull_up_down = gpio.PUD_DOWN)
  
 # Adicionando um evento ao GPIO 23 na mudança RISING 0V[LOW] -> 3.3V[HIGH]
-gpio.add_event_detect(PIN, gpio.RISING)
+gpio.add_event_detect(Porta, gpio.BOTH)
  
  
 while True:
     try:
-        if gpio.event_detected(PIN):
-            action_press_button(PIN)
-            gpio.remove_event_detect(PIN)
+        if gpio.event_detected(Porta):
+            action_event_button(Porta)
+            #gpio.remove_event_detect(PIN)
         
         else:
                     print("Botão Desligado")
